fc419052969c590fe917c4d15dd730b93e54eeae
Test <slavasvssvssvs@gmail.com>
2023-01-28 04:32:21 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int score = 100; // Счет клиента
+        int deposit = 1000; // депозит
+        boolean dp = true;
+
+        int bonus;
+
+        if (deposit >= 1000) {
+            bonus = deposit/100;
+        } else {
+            bonus = 0;
+        }
+        int all = score + deposit + bonus;
+        System.out.println("Ваш счет общий счет:" + all + "руб");
+        System.out.println("Ваш бонус:" + bonus + "руб");
+    }
+}
